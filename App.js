9a61304a224d86932889ed220c379cf50517b1a3
양선우 <doawishfor@naver.com>
2023-05-26 23:19:08 +0900

    commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-<script src="http://localhost:8097"></script>;
-import React, {useState} from 'react';
+import React from 'react';
 
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 function App() {
   const today = new Date();
   console.log('양선우:' + today);
 
   return (
-    <SafeAreaView>
-      <DateHead date={today} />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <DateHead date={today} />
+        <Empty />
+        <AddTodo />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
 
 export default App;
